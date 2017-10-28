50b89a0710b5cf7bf7a619aa5b5e2e3976be4e81
dcaban <davidjcaban@gmail.com>
2017-10-28 10:56:42 -0400

    post almost done

--- server.js
+++ server.js
@@ -8,21 +8,31 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 //variable arrays
-var reservation {
-    name: "",
-    phone: "",
-    email: "",
-    ID: ""
-};
+var reservation = [];
 
-var tables {
-    name: "",
-    phone: "",
-    email: "",
-    ID: "",
-};
+var wait = [{
+    name: "hey",
+    phone: "hello",
+    email: "whats up",
+    ID: "43"
+}];
 
-app.get("/", function(req, res))
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"))
+});
+app.get("/make", function(req, res) {
+    res.sendFile(path.join(__dirname, "make.html"))
+});
+
+app.get("/view", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"))
+});
+app.get("/wait", function(req, res) {
+    res.json(wait);
+});
+app.post("/api/tables", function(req, res) {
+    reservation.push(req.body)
+});
 
 
 // Sets up the Express app to handle data parsing
